1b1327a581246dff61ef90eed7181071b69f8bc7
Olof-Joachim Frahm <olof@macrolet.net>
2014-08-08 10:06:28 +0100

    Custom colours.

--- config.def.h
+++ config.def.h
@@ -6,12 +6,12 @@
 static Bool topbar = True;                  /* -b  option; if False, dmenu appears at bottom */
 static const char *font = NULL;             /* -fn option; default X11 font or font set      */
 static const char *prompt = NULL;           /* -p  option; prompt to the elft of input field */
-static const char *normbgcolor = "#222222"; /* -nb option; normal background                 */
+static const char *normbgcolor = "#111111"; /* -nb option; normal background                 */
 static const char *normfgcolor = "#bbbbbb"; /* -nf option; normal foreground                 */
-static const char *selbgcolor  = "#005577"; /* -sb option; selected background               */
+static const char *selbgcolor  = "#222222"; /* -sb option; selected background               */
 static const char *selfgcolor  = "#eeeeee"; /* -sf option; selected foreground               */
-static const char *outbgcolor  = "#00ffff";
-static const char *outfgcolor  = "#000000";
+static const char *outbgcolor  = "#222222";
+static const char *outfgcolor  = "#eeeeee";
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 0;
 
